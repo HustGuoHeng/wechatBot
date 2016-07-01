c48bddd2e0c0c370c5ab4ef68fcd05e827a6c58b
GuoHeng <guoheng_sx@iqiyi.com>
2016-07-01 23:48:15 +0800

    扫描二维码成功获取200状态

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
     $wechatBot = new wechatBot();
     $wechatBot->getUuid();
 
@@ -7,12 +8,18 @@
     ob_flush();
     flush();
     
+    while ($wechatBot->waitForLogin() != 200) {
+    	true;
+    }
 
 class wechatBot
 {
-    public $uuid;
+    public $uuid = false;
     public $redirectUri;
     public $baseUri;
+
+    protected $waitForCheck = false; //用于标志用户是否扫描
+    protected $tid = 1;
     public function __construct()
     {
     }
@@ -22,16 +29,21 @@ class wechatBot
     public function getUuid()
     {
         $getUuidUrl = "https://login.weixin.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Fwx.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=en_US&_=1452859503801";
-        $result = self::curlRequest($getUuidUrl);
-        $result = preg_match('/uuid = "([a-zA-Z0-9_]*={2})"/', $result, $matches);
-        $uuid =  $matches['1'];
-        $this->uuid = $uuid;
+
+        while (!$this->uuid) {
+	        $result = self::curlRequest($getUuidUrl);
+	        $result = preg_match('/uuid = "([a-zA-Z0-9_]*={2})"/', $result, $matches);
+	        $uuid =  $matches['1'];
+	        $this->uuid = $uuid;
+        }
+
     }
     /**
      * 获取二维码
      */
     public function getQRcodeUrl()
-    {                                                                                                                                        $url = "https://login.weixin.qq.com/qrcode/" . $this->uuid;
+    {   
+    	$url = "https://login.weixin.qq.com/qrcode/" . $this->uuid;
         return $url;                                                                                                                 
     }
     /**
@@ -40,17 +52,23 @@ class wechatBot
     public function waitForLogin()
     {
         $now_time = time();
-        $url = "https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid=" . $this->uuid . "&tip=1&r=" . $now_time . "&_=1452859503803";
-        $result = $this->curlRequest($url, false, array(), 30);
+        $url = "https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid=" . $this->uuid . "&tip=" . $this->tid . "&r=" . $now_time . "&_=1452859503803";
+        $result = $this->curlRequest($url, false, array(), 20);
         preg_match('/window.code=([0-9]*)/',$result,$matches);
-        $code = $matches['1'];
-        if ($code == '201'){
+        
+        @$code = $matches[1];
+        if ($code == '201' && !$this->waitForCheck){
             echo "<br>";
+            $this->waitForCheck =  true;
+            $this->tid = 0;
             echo "扫描成功，请在手机确认登录！";
-     } else if ($code == '200') {
+            ob_flush();
+            flush();
+     	} else if ($code == '200') {
             echo "<br>";
             echo "登录成功，正在进行下一步,请稍后：";
             preg_match('/window.redirect_uri=\"(\S*)\"/',$result,$matches);
+            print_r($result);
             $redirectUri = $matches['1'];
             $this->redirectUri = $redirectUri;
         }else if ($code == '408') {
